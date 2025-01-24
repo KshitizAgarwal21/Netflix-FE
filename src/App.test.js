c3169a13ddf4d4fcfc4006274279d8ed6c0c1645
pratima0809 <pratima8926@gmail.com>
2025-01-24 17:56:26 +0530

    added Comment

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
+//Hiii world
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
